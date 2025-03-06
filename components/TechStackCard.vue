71bd130f55178f9f7b12e32f3c0d49f71df6b7e8
MadeYoga <vngla21@gmail.com>
2025-03-06 12:23:18 +0800

    Update

---
+++ components/TechStackCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hover:bg-slate-800 transition-all ease-in-out p-3 rounded-md border border-gray-400 flex flex-col items-center justify-center">
+    <slot></slot>
+
+    <div class="text-sm mt-1">{{ props.title }}</div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  "title": String,
+})
+</script>
